cae26ca68f99a33268e26faacb4efcaa8304fcf1
Brian Maitho <Bmaitho@gmail.com>
2024-04-28 20:48:22 +0300

    THE FLATIRON BANK

---
+++ bank-of-flatiron/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Purchasetable from './TheTable';
+
+function App() {
+  return (
+    <div className="App">
+    
+  <Purchasetable/>
+  </div>
+    
+  );
+}
+
+
+export default App;
